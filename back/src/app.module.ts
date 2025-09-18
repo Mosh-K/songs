ffe47ef0a55d42b6f954bdd5ad4430eb33f590b5
Mosh-K <158998622+Mosh-K@users.noreply.github.com>
2025-09-18 20:23:36 +0300

    minor typo fix

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DB_CONFIG } from './DB_CONFIG';
+import { DB_CONFIG } from './dbConfig';
 import { SongsModule } from './songs/songs.module';
 
 @Module({
